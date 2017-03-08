868d189d2d5f1b1d47c5d982b70bec7d435f4d72
Tom Langton <tom@pulpinfo.com>
2017-03-08 15:52:41 -0500

    good for math

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -27,96 +27,84 @@ trainOneMins = 100;
 frequencyMins = 60;
 
 
-var nowMinsAfterMdnt = (moment().get('hour') * 60 ) + moment().get('minute');
+
 
 
 $("#submit").click(function(){
+// alert("Value: " + $("#in_1stTrain").val
+// var trainOneMins = $("#in_1stTrain").val().trim();
 
-        // alert("Value: " + $("#in_1stTrain").val
-        var trainOneMins = $("#in_1stTrain").val().trim();
-        var frequencyMins = $("#in_frequency").val().trim();
-        var trainName = $("#in_trainName").val().trim();
-        var destination = $("#in_dest").val().trim();
+var train1Input =  moment($("#in_1stTrain").val().trim(), "hmm").format("HH:mm");
 
+var train1timeSplit = train1Input.split(":");
+//calcs minutes since midnight till first train arrives
+var trainOneMins = parseInt((train1timeSplit[0] * 60)) + parseInt(train1timeSplit[1]);
 
-        firebaseRef.push({
-            firstTrain: trainOneMins,
-            nameTrain: trainName,
-            dest: destination,
-            frequency: frequencyMins,
-            timestampCreated: firebase.database.ServerValue.TIMESTAMP
-        });
+var frequencyMins = $("#in_frequency").val().trim();
+var trainName = $("#in_trainName").val().trim();
+var destination = $("#in_dest").val().trim();
 
 
 
-        var difNowFirstMins = nowMinsAfterMdnt - trainOneMins;
-        console.log("dif now from first " + difNowFirstMins);
+firebaseRef.push({
+    train1Input: train1Input,
+    firstTrain: trainOneMins,
+    nameTrain: trainName,
+    dest: destination,
+    frequency: frequencyMins,
+    timestampCreated: firebase.database.ServerValue.TIMESTAMP
+});
 
-        var diffOverFrequency = difNowFirstMins / frequencyMins;
-        console.log("dif over frequency " + diffOverFrequency);
+$("#in_1stTrain").val('');
+$("#in_frequency").val('');
+$("#in_trainName").val('');
+$("#in_dest").val('');
 
-        var modDiff_Freq = (difNowFirstMins % frequencyMins) / frequencyMins ;
-        console.log("mod dif over frequency " + modDiff_Freq);
+});
 
-        var nextTrainMins = frequencyMins - ( frequencyMins * modDiff_Freq );
-        console.log("next Train Mins " + nextTrainMins)
+//output table
 
-        var timeNextTrain = moment().add(nextTrainMins, "minutes").format("HH:mm");
-        console.log("time Next Train " + timeNextTrain);
-
-
-        $("#firstTrain").html("First train (mins after midnite): " + trainOneMins);
-
-        $("#frequency").html("Frequency: " + frequencyMins);
-
-        $("#nextTrain").html("Next Train: " + timeNextTrain);
-    });
-
-firebaseRef.on("value", function(snapshot) {
-
-        // Print the initial data to the console.
-        console.log(snapshot.val());
-
-        $("#firstTrain").html(snapshot.val().firstTrain);
-        $("#frequency").html(snapshot.val().frequency);
-         // $("#nextTrain").html(snapshot.val().firstTrain);
-
-
-      // If any errors are experienced, log them to console.
-  }, function(errorObject) {
-      console.log("The read failed: " + errorObject.code);
-  });
-
-
-$(".well").append("<table><tr><th>Train Name:</th><th>Destination:</th><th>First Train:</th><th>Next Train:</th><th>Next Arrival:</th><th>Frequency:</th></tr>");
-
+$(".well").append("<table><tr><th>Train Name:</th><th>Destination:</th><th>First Train:</th><th>Next Train:</th><th>Next Arrival:</th><th>Frequency:</th><th>Edit</th></tr>");
 
 firebaseRef.ref.orderByChild( "timestampCreated").on("child_added", function(snapshot) {
 
-    console.log(snapshot);
+    // console.log(snapshot);
+    var nowMinsAfterMdnt = (moment().get('hour') * 60 ) + moment().get('minute');
+    var timestampCreated = snapshot.val().timestampCreated;
+    var frequency = snapshot.val().frequency;
+    var nameTrain = snapshot.val().nameTrain;
+    var firstTrain = snapshot.val().firstTrain;
+    var train1Input = snapshot.val().train1Input;
+    var dest = snapshot.val().dest;
 
- var difNowFirstMins = nowMinsAfterMdnt - (snapshot.val().firstTrain);
- var diffOverFrequency = difNowFirstMins / (snapshot.val().frequency);
- var modDiff_Freq = (difNowFirstMins % (snapshot.val().frequency)) / (snapshot.val().frequency) ;
- var nextTrainMins = (snapshot.val().frequency) - ( (snapshot.val().frequency) * modDiff_Freq );
- var timeNextTrain = moment().add(nextTrainMins, "minutes").format("HH:mm");
+
+if (nowMinsAfterMdnt > firstTrain) {
+    var diffNowFirstMins = nowMinsAfterMdnt - firstTrain;
+    var diffOverFrequency = diffNowFirstMins / frequency;
+    var modDiff_Freq = (diffNowFirstMins % frequency) / (frequency) ;
+    var nextTrainMins = (frequency) - ( frequency * modDiff_Freq );
+    var timeNextTrain = moment().add(nextTrainMins, "minutes").format("HH:mm");
+} else {
+
+    // future frist train
+    var nextTrainMins = firstTrain - nowMinsAfterMdnt;
+    var timeNextTrain = train1Input;
+
+}
 
 
-    $(".well").append("<tr>");
-    $(".well").append("<td>"+snapshot.val().nameTrain+"</td>");
 
-    $(".well").append("<td>"+ (snapshot.val().dest)+"</td>");
+    console.log(timestampCreated);
 
-    $(".well").append("<td>"+snapshot.val().firstTrain+"</td>");
-
+    $(".well").append("<tr class:'edit'>");
+    $(".well").append("<td>"+ nameTrain +"</td>");
+    $(".well").append("<td>"+ dest +"</td>");
+    $(".well").append("<td>"+ train1Input +"</td>");
     $(".well").append("<td>"+ timeNextTrain+"</td>");
-
-    $(".well").append("<td>"+ (nextTrainMins)+" minutes</td>");
-
-      $(".well").append("<td>"+ (snapshot.val().frequency)+"</td>");
-
+    $(".well").append("<td>"+ nextTrainMins +" minutes</td>");
+    $(".well").append("<td>"+ frequency +"</td>");
+    $(".well").append("<td  id:" + timestampCreated +">Edit "+ timestampCreated +" </td>");
     $(".well").append("</tr>");
-
 });
 
 $(".well").append("<table>");
@@ -125,9 +113,9 @@ $(".well").append("<table>");
 
 
 
-
-
-
-
-
-
+$(".edit").on("click", function(){
+ alert( "Handler for .click() called." );
+ butID = $(this).attr('id');
+ console.log (butID);
+    // answer();
+});
