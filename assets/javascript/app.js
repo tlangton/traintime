185215550cfa29350e70af4159f863196f2481db
Tom Langton <tom@pulpinfo.com>
2017-03-09 10:05:31 -0500

    links working - needs delete and edit functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,8 +13,6 @@ firebase.initializeApp(config);
 var firebaseRef = firebase.database().ref("trains");
 
 
-var nowMins = 0;
-// now mins since midnight
 var trainOneMins = 0;
 // first train arrival mins
 var frequencyMins = 0;
@@ -22,20 +20,13 @@ var frequencyMins = 0;
 var trainName;
 var destination;
 
-// nowMins = 200;
-trainOneMins = 100;
-frequencyMins = 60;
 
 
-
-
-
+//new train button entry
 $("#submit").click(function(){
-// alert("Value: " + $("#in_1stTrain").val
-// var trainOneMins = $("#in_1stTrain").val().trim();
 
-var train1Input =  moment($("#in_1stTrain").val().trim(), "hmm").format("HH:mm");
-
+// var train1Input =  moment($("#in_1stTrain").val().trim(), "hmm").format("HH:mm");
+var train1Input =  $("#in_1stTrain").val().trim();
 var train1timeSplit = train1Input.split(":");
 //calcs minutes since midnight till first train arrives
 var trainOneMins = parseInt((train1timeSplit[0] * 60)) + parseInt(train1timeSplit[1]);
@@ -43,7 +34,6 @@ var trainOneMins = parseInt((train1timeSplit[0] * 60)) + parseInt(train1timeSpli
 var frequencyMins = $("#in_frequency").val().trim();
 var trainName = $("#in_trainName").val().trim();
 var destination = $("#in_dest").val().trim();
-
 
 
 firebaseRef.push({
@@ -62,9 +52,18 @@ $("#in_dest").val('');
 
 });
 
+
+//clear train entry fields
+$("#clear").click(function(){
+$("#in_1stTrain").val('');
+$("#in_frequency").val('');
+$("#in_trainName").val('');
+$("#in_dest").val('');
+});
+
 //output table
 
-$(".well").append("<table><tr><th>Train Name:</th><th>Destination:</th><th>First Train:</th><th>Next Train:</th><th>Next Arrival:</th><th>Frequency:</th><th>Edit</th></tr>");
+$(".well").html("<table><tr><th>Train Name:</th><th>Destination:</th><th>First Train:</th><th>Next Train:</th><th>Next Arrival:</th><th>Frequency:</th></tr>");
 
 firebaseRef.ref.orderByChild( "timestampCreated").on("child_added", function(snapshot) {
 
@@ -73,49 +72,58 @@ firebaseRef.ref.orderByChild( "timestampCreated").on("child_added", function(sna
     var timestampCreated = snapshot.val().timestampCreated;
     var frequency = snapshot.val().frequency;
     var nameTrain = snapshot.val().nameTrain;
-    var firstTrain = snapshot.val().firstTrain;
+    var firstTrainMinsAfterMdnt = snapshot.val().firstTrain;
     var train1Input = snapshot.val().train1Input;
     var dest = snapshot.val().dest;
+    var key = snapshot.key;
 
-
-if (nowMinsAfterMdnt > firstTrain) {
-    var diffNowFirstMins = nowMinsAfterMdnt - firstTrain;
-    var diffOverFrequency = diffNowFirstMins / frequency;
-    var modDiff_Freq = (diffNowFirstMins % frequency) / (frequency) ;
-    var nextTrainMins = (frequency) - ( frequency * modDiff_Freq );
-    var timeNextTrain = moment().add(nextTrainMins, "minutes").format("HH:mm");
-} else {
-
-    // future frist train
-    var nextTrainMins = firstTrain - nowMinsAfterMdnt;
+    //calcs
+    //if first train arrived before now today
+    if (nowMinsAfterMdnt > firstTrainMinsAfterMdnt) {
+        var diffNowFirstMins = nowMinsAfterMdnt - firstTrainMinsAfterMdnt  ;
+        var diffOverFrequency = diffNowFirstMins / frequency;
+        var modDiff_Freq = (diffNowFirstMins % frequency) / (frequency) ;
+        var nextTrainMins =   Math.floor((frequency) - ( frequency * modDiff_Freq ));
+        var timeNextTrain = moment().add(nextTrainMins, "minutes").format("HH:mm");
+    } else {
+    // future first train - will arrive after now
+    var nextTrainMins = firstTrainMinsAfterMdnt - nowMinsAfterMdnt;
     var timeNextTrain = train1Input;
-
 }
-
-
-
-    console.log(timestampCreated);
-
-    $(".well").append("<tr class:'edit'>");
-    $(".well").append("<td>"+ nameTrain +"</td>");
-    $(".well").append("<td>"+ dest +"</td>");
-    $(".well").append("<td>"+ train1Input +"</td>");
-    $(".well").append("<td>"+ timeNextTrain+"</td>");
-    $(".well").append("<td>"+ nextTrainMins +" minutes</td>");
-    $(".well").append("<td>"+ frequency +"</td>");
-    $(".well").append("<td  id:" + timestampCreated +">Edit "+ timestampCreated +" </td>");
-    $(".well").append("</tr>");
+var $trainRow = $("<tr>")
+$trainRow.addClass('train-row')
+$trainRow.attr('id', key)
+$trainRow.css({cursor: 'pointer'})
+$trainRow.append("<td class='train-name'>"+ nameTrain +"</td>");
+$trainRow.append("<td class='train-dest'>"+ dest +"</td>");
+$trainRow.append("<td class='train-first'>"+ train1Input +"</td>");
+$trainRow.append("<td>"+ timeNextTrain+"</td>");
+$trainRow.append("<td>"+ nextTrainMins +" minutes</td>");
+$trainRow.append("<td class='train-frequency'>"+ frequency +" minutes</td>");
+$("table").append($trainRow);
 });
 
-$(".well").append("<table>");
+$(".well").on("click", ".train-row", function(){
+   butID = $(this).attr('id');
+   console.log (butID);
+
+   var trainName = $(this).find('.train-name').text()
+   var trainDest = $(this).find('.train-dest').text()
+   var trainFirst = $(this).find('.train-first').text()
+   var trainFrequency = $(this).find('.train-frequency').text()
+var trainFreqSplit = trainFrequency.split(" ");
+var trainFreqFix = parseInt((trainFreqSplit[0]))
 
 
+   $("#in_trainName").val(trainName);
+   $("#in_dest").val(trainDest);
+   $("#in_1stTrain").val(trainFirst);
+   $("#in_frequency").val(trainFreqFix);
 
-
-
-$(".edit").on("click", function(){
- alert( "Handler for .click() called." );
- butID = $(this).attr('id');
- console.log (butID);
-    // answer();
+ // alert(trainName)
 });
+
+
+// $(".well").click(function(){
+//     alert("The paragraph was clicked.");
+// });
